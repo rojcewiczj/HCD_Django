87aa8a54619319ce5016a38ed91e16311e989e01
John Rojcewicz <rojcewiczj@gmail.com>
2021-09-18 16:27:27 -0500

    basic structure is coming together, learning how to use conditionals and loops

--- hcd_app/form_builder/views.py
+++ hcd_app/form_builder/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    forms = {
+         'HCD_lead_mitigation': [{"question":"where do you live?", "answer": "memphis"}]
+         }
+    
+    return render(request, "form_builder/index.html", {
+        'building': True,
+        "forms":  forms
+    })
